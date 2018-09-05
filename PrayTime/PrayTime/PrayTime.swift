72d7c92039d32f6e0d97381da09445a31e7d7d6c
“Atif <atif.gcucs@gmail.com>
2018-09-05 11:02:01 +0400

    Code commit

---
+++ PrayTime/PrayTime/PrayTime.swift
@@ -0,0 +1,14 @@
+//
+//  PrayTime.swift
+//  PrayTime
+//
+//  Created by Atif on 05/09/2018.
+//  Copyright © 2018 atif.gcucs@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class PrayTime {
+    // Calculation Methods
+    
+}
